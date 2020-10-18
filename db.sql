af1f01b122209c797123f9d424449fdc45c58e2d
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-10-18 15:12:07 -0500

    fixing sql tables sentences

--- db.sql
+++ db.sql
@@ -1,11 +1,19 @@
 CREATE TABLE  `roles` (
-  id int(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  idRole int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   roleName varchar(255) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE  `users` (
-  id int(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  idUser int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   firstName varchar(255) NOT NULL,
   lastname varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
   active BOOLEAN DEFAULT false
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `users_role` (
+  idUser_Role int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	idUserUR int
+	idRoleUR int 
+	FOREIGN KEY 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
