73e22cfeecd6be1c4b6aec6d77720afef24007f6
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-10-18 21:31:05 -0500

    adding cors

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+var cors = require('cors')
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
