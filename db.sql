15eb221cb6be24a44f684065597e4979c49591dc
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-10-18 21:49:43 -0500

    sql stament typo

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ CREATE TABLE  roles (idRole int NOT NULL PRIMARY KEY AUTO_INCREMENT, roleName va
 CREATE TABLE  users (
   idUser int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   firstName varchar(255) NOT NULL,
-  lastname varchar(255) NOT NULL,
+  lastName varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
   active BOOLEAN DEFAULT false
 ) ;
