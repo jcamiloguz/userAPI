b6c8c02d47857a6d54ba1e805dafa469140c6c80
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-10-18 20:18:37 -0500

    heroku ENV

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "Teck3601",
-  DB: "userAPI"
+  HOST: "us-cdbr-east-02.cleardb.com",
+  USER: "bd06860c1ec903",
+  PASSWORD: "1c57ca17",
+  DB: "heroku_702c32bb98aa0a3"
 };
